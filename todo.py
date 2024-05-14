44228d627244ab40845ed34c99393a1fa8e9b67d
Shreeyanshi Gautam <67256100+shreeyanshi-2111@users.noreply.github.com>
2024-05-14 16:15:30 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,3 +1,16 @@
+def add_task(name, description, priority, time, status):
+    todo = load_tasks()
+    task_id = len(tasks) + 1
+    task = {
+        'id': task_id,
+        'name': name,
+        'description': description,
+        'priority': priority,
+        'time': time,
+        'status': status
+    }
+    todo.append(task)
+    print("Task added successfully.")
 def delete_task(tasks):
     tasks = get_tasks()
     if task_id >= 0 and task_id < len(tasks):
